c9bf80f868ec825b1db445059c7da79bb2554768
Santiago Reyes PC CORP <santiago.reyes@cun.edu.co>
2025-12-29 10:16:47 -0500

    Se arreglan algunos errores con la conexiones a supabase

--- scripts/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// Configuración de Supabase
-const SUPABASE_URL = 'https://iryiobhmmvdnvwfovuye.supabase.co';
-const SUPABASE_KEY = 'sb_publishable__ebSt_oeNFqErCtk1ZUfSg_P0V6vNt7';
-
-const supabase = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-)
-
